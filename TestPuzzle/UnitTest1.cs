6564b64a2f2cc2f5d0902bcb979240e2fb3caafd
Ross Ylitalo <ross.ylitalo@gmail.com>
2018-05-19 20:24:50 -0400

    Added unit test that checks that the cCurPostion class is working.

--- TestPuzzle/UnitTest1.cs
+++ TestPuzzle/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2dPuzzleGenerator.sudoLib;
 
 namespace TestPuzzle
 {
@@ -7,8 +8,22 @@ namespace TestPuzzle
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCurPositionClass()
         {
+            cCurPosition cur = new cCurPosition();
+            while (!cur.EndPosition)
+            {
+                cur++;
+            }
+            Assert.AreEqual(g.PSIZE - 1, cur.Row, "Row is not at its End." );
+            Assert.AreEqual(g.PSIZE - 1, cur.Col, "Col is not at its End." );
+
+            while (!cur.StartPosition)
+            {
+                cur--;
+            }
+            Assert.AreEqual(0, cur.Row, "Row is not at its Beginning." );
+            Assert.AreEqual(0, cur.Col, "Col is not at its Beginning." );
         }
     }
 }
